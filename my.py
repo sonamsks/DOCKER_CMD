49294e8dc06f4a1dafa78e72226d1a7f51c9c41a
sonamsks <71845005+sonamsks@users.noreply.github.com>
2021-10-27 14:11:33 +0530

    Create my.py

---
+++ my.py
@@ -0,0 +1,17 @@
+x = print()
+len()  /  print( len() )
+
+
+import sys
+x = sys.stdin   //* read the file *//
+print(x)
+
+import sys
+x = sys.argv
+print(x)             //* vim my.py
+                        python3 my.py or python3 my.py sonam *//
+  
+  
+  import sys
+  x = sys.argv[0]   //* zero always pick the 1st argu and one(1) pick the all argu lenght value *// 
+  print( len(x) )
